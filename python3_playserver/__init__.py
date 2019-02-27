5faf2ff241071e053d32d0cbb32c50eac7c72052
Syntaxp_403 <vnosfilds@gmail.com>
2019-02-28 01:40:05 +0700

    __init__

---
+++ python3_playserver/__init__.py
@@ -0,0 +1,3 @@
+from .config import key,url_getpic,url_image,header,proxy,titlechang,key,userid,server_id,url_submitpic,LOGSET,maxvote,VPN,USERVPN,PASSVPN,ver_,src_path
+from .request_playserver import GETIMAGE,POSTIMAGE
+from .DELAY import SETDELAY,SLEEPDELAY
