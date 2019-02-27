28d3e81569a02053869e2996a4e65197d37086b7
Syntaxp_403 <vnosfilds@gmail.com>
2019-02-28 01:39:29 +0700

    Create config.py

---
+++ python3_anticaptcha/config.py
@@ -0,0 +1,6 @@
+create_task_url = ("https://api.anti-captcha.com/createTask")
+get_result_url = ("https://api.anti-captcha.com/getTaskResult")
+get_balance_url = ("https://api.anti-captcha.com/getBalance")
+incorrect_captcha_url = ("https://api.anti-captcha.com/reportIncorrectImageCaptcha")
+get_queue_status_url = ("https://api.anti-captcha.com/getQueueStats")
+header = { "Accept": "application/json","Content-Type": "application/json"}
