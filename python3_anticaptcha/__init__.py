e1781edefab88b748558644f2a8d87f2b58efdea
Syntaxp_403 <vnosfilds@gmail.com>
2019-02-28 01:39:00 +0700

    __init__

---
+++ python3_anticaptcha/__init__.py
@@ -0,0 +1,2 @@
+from .config import create_task_url, get_result_url, get_balance_url, incorrect_captcha_url,get_balance_url,header,incorrect_captcha_url
+from request_anitcapcha import GETCAPCHA,GETbalance,reportIncorrectImageCaptcha
