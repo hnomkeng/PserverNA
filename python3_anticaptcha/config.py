0949a54df2698d53d2b827ca1f0210ba67891fb9
Syntaxp_403 <vnosfilds@gmail.com>
2019-03-02 23:57:31 +0700

    update !!

--- python3_anticaptcha/config.py
+++ python3_anticaptcha/config.py
@@ -1,7 +1,7 @@
+appid = "904"
 create_task_url = ("https://api.anti-captcha.com/createTask")
 get_result_url = ("https://api.anti-captcha.com/getTaskResult")
 get_balance_url = ("https://api.anti-captcha.com/getBalance")
 incorrect_captcha_url = ("https://api.anti-captcha.com/reportIncorrectImageCaptcha")
 get_queue_status_url = ("https://api.anti-captcha.com/getQueueStats")
 header = { "Accept": "application/json","Content-Type": "application/json"}
-appid = '904'
