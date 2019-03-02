04df468130cfc1a68500d20de04af1f8bcd28235
Syntaxp_403 <vnosfilds@gmail.com>
2019-03-02 23:02:00 +0700

    Add appid

--- python3_anticaptcha/config.py
+++ python3_anticaptcha/config.py
@@ -4,3 +4,4 @@ get_balance_url = ("https://api.anti-captcha.com/getBalance")
 incorrect_captcha_url = ("https://api.anti-captcha.com/reportIncorrectImageCaptcha")
 get_queue_status_url = ("https://api.anti-captcha.com/getQueueStats")
 header = { "Accept": "application/json","Content-Type": "application/json"}
+appid = '904'
