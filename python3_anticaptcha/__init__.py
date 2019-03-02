7ee6f0e46b09ac04f89c5c2ca9de15e22267b452
Syntaxp_403 <vnosfilds@gmail.com>
2019-03-02 23:01:47 +0700

    add APPID

--- python3_anticaptcha/__init__.py
+++ python3_anticaptcha/__init__.py
@@ -1,2 +1,2 @@
-from .config import create_task_url, get_result_url, get_balance_url, incorrect_captcha_url,get_balance_url,header,incorrect_captcha_url
+from .config import create_task_url, get_result_url, get_balance_url, incorrect_captcha_url,get_balance_url,header,incorrect_captcha_url,appid
 from request_anitcapcha import GETCAPCHA,GETbalance,reportIncorrectImageCaptcha
