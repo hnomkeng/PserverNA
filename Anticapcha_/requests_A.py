141190e79e6c9476a739a95148f760e4078b9b67
Syntaxp_403 <vnosfilds@gmail.com>
2019-03-03 14:38:17 +0700

    fix

--- Anticapcha_/requests_A.py
+++ Anticapcha_/requests_A.py
@@ -1,9 +1,10 @@
 import requests,json,time,threading,sys
 from data_anti import get_balance_url,header,create_task_url,get_result_url,Appid,incorrect_captcha_url
 
-
+e = threading.Event()
 def GETbalance(key):
     data = {'clientKey':key}
+    e.wait(timeout=0.1)
     p = requests.post(get_balance_url,headers=header,json=data).json()
     return p['balance']
 
@@ -24,7 +25,7 @@ def GETCAPCHA(key,base64):
     "softId":int(Appid),
     "languagePool":"en"
     }
-
+    e.wait(timeout=0.1)
     createTask  = requests.post(create_task_url,json=Taskdata).json()
     if createTask['errorId'] != 1:
         TaskID = {
@@ -32,12 +33,12 @@ def GETCAPCHA(key,base64):
             "taskId": createTask['taskId']
             }
         for timeout in range(60):
+            e.wait(timeout=0.1)
             captcha_id = requests.post(get_result_url, json = TaskID).json()
             if captcha_id['status'] != 'processing':
                 captcha = {'status':True,'text':captcha_id['solution']['text'],'cost':captcha_id['cost'],'taskId':createTask['taskId']}
                 return captcha
             else:
-                e = threading.Event()
                 e.wait(timeout=3)
     else:
         return captcha['status':False]
