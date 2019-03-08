038f2ecacd51dd5a548e1489dd52423bebd33994
Syntaxp_403 <vnosfilds@gmail.com>
2019-03-09 01:15:25 +0700

    fix timeout

--- Playserver_/Playserver.py
+++ Playserver_/Playserver.py
@@ -14,6 +14,7 @@ class POST_ANTICAPTCHA:
         self.method_1()
     def method_1(self):
         def run(self):
+            e = threading.Event()
             self.true = 0
             self.fail = 0
             self.persen = 0
@@ -27,6 +28,7 @@ class POST_ANTICAPTCHA:
                     public_ = 0
                     #start threading
                     threading.Thread(target = PserverNA, args = (self,public_proxy,public_)).start()
+                    e.wait(timeout=0.5)
                     self.proxywork += 1
             if int(private) == 1:
                 with open('control/private.txt','r') as loadVPN:
@@ -37,6 +39,7 @@ class POST_ANTICAPTCHA:
                         private_ = 1
                         self.proxywork += 1
                         threading.Thread(target = PserverNA, args = (self,private_proxy,private_)).start()
+                        e.wait(timeout=0.5)
         def PserverNA(self,proxyX,moade):
             My_proxy = re.findall( r'[0-9]+(?:\.[0-9]+){3}', proxyX['http'] )
             if int(moade) == 1:
@@ -45,7 +48,6 @@ class POST_ANTICAPTCHA:
             PRoxyDie = 0
             startloop = 0
             waitkick = 0
-            e = threading.Event()
             while startloop == 0:
                 try:
                     titlechang(self)
--- Playserver_/requests_P.py
+++ Playserver_/requests_P.py
@@ -3,10 +3,10 @@ from data import url_image, url_submitpic, header, url_getpic,requesttimeout
 e = threading.Event()
 def GETIMAGE(proxy):
     try:
-        e.wait(timeout=0.8)
+        e.wait(timeout=0.5)
         requestid = (requests.post(url_getpic,verify=False, timeout=int(requesttimeout), headers=header, proxies=proxy)).json()
         IMAGE_ID = requestid['checksum']
-        e.wait(timeout=0.8)
+        e.wait(timeout=0.5)
         IMAGECT = requests.post(url_image + IMAGE_ID,verify=False, timeout=int(requesttimeout),  headers=header, proxies=proxy)
         base64pic = base64.b64encode(IMAGECT.content).decode('utf-8')
         if base64pic.find('iVBORw0KGgoAAAANSUhE') > -1:
@@ -20,7 +20,7 @@ def GETIMAGE(proxy):
 def POSTIMAGE(self,votedata, proxy):
     for timeout in range(60):
         try:
-            e.wait(timeout=0.8)
+            e.wait(timeout=0.5)
             vote = (requests.post(url_submitpic,verify=False, timeout=int(requesttimeout),  headers=header, data=votedata, proxies=proxy)).json()
             if vote['success'] == True:
                 self.true += 1
